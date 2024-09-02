2da0a5fb8568db8416537477d7d231ac2279cd32
saumittras <saumittra.eee@gmail.com>
2024-09-02 22:33:44 +0600

    added fast two videos code

--- moduleCode/string_video_1.js
+++ moduleCode/string_video_1.js
@@ -1,1 +1,22 @@
-sdf;
+// 4 way to declear of string
+
+/**
+ * 
+onst country = "Bangladesh";
+const divition = 'Newkhali';
+const district = `B-Baria`;
+const thana = new String("Drama");
+ */
+
+const country = "Bangladesh";
+const divition = "Newkhali";
+const district = `B-Baria`;
+const thana = new String("Drama");
+
+// string operation
+
+const capital = "Dhaka";
+console.log(capital.length);
+
+// array element are muteable but string element is not muteable
+// array1[3]="something"; muteable but string1[4] = 's'; not muteable
---
+++ moduleCode/string_video_2.js
@@ -0,0 +1,21 @@
+const school = "Rajuk Uttra Model School";
+
+// console.log(school);
+
+const subject = "Chemistry";
+const book = "chemistry";
+// Normal to compare
+if (subject === book) {
+  console.log("match the book");
+} else {
+  console.log("does not match");
+}
+
+// conver the string to small letter toLowercase() function
+// conver the string to capital letter toUppercase() function
+
+if (subject.toLowerCase() === book.toLowerCase()) {
+  console.log("match the book");
+} else {
+  console.log("does not match");
+}
